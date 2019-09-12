bfda7267cf174eab3376289a511e6f80096ef6c2
Sarah B <smblair87@gmail.com>
2019-09-11 20:15:38 -0700

    Trying another SSL npm package

--- ssl.js
+++ ssl.js
@@ -1,11 +1,29 @@
-var express = require('express');
-var sslRedirect = require('strong-ssl-redirect');
-var app = express();
-var environment = 'production'  /* 'other' 'development', 'production'*/
-app.use(sslRedirect({
-  environment,
-  www: true,
-  status: 301
-}));
-
-// If you want to force the request to redirect from https://google.com to https://www.google.com set the www property to true like so. ```javascript `www: true`
+const express = require('express');
+const app     = express();
+const http    = require('http');
+const https   = require('https');
+const fs      = require('fs');
+ 
+const sslOptions  = {
+  key: fs.readFileSync('./ssl/privatekey.pem'),
+  cert: fs.readFileSync('./ssl/certificate.pem')
+};
+ 
+const server    = http.createServer(app);
+const sslServer = https.createServer(sslOptions, app);
+ 
+const redirectHttps = require('express-redirect-https');
+let redirectOptions = {
+  allowForwardForHeader: true,
+  httpsPort: 3043
+};
+ 
+  
+// Or for a single
+app.get('/', redirectHttps(redirectOptions), function(request, response, next) {
+  response.send('https://portfolio.sarahbeecroft.me');
+});
+ 
+ 
+server.listen(3000);
+sslServer.listen(3043);
