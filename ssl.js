5fff9c1088ee0988b72b82ab05e192a99f79d7c9
Sarah B <smblair87@gmail.com>
2019-09-13 18:36:12 -0700

    Adding package.json

--- ssl.js
+++ ssl.js
@@ -1,29 +1,12 @@
-const express = require('express');
-const app     = express();
-const http    = require('http');
-const https   = require('https');
-const fs      = require('fs');
- 
-const sslOptions  = {
-  key: fs.readFileSync('./ssl/privatekey.pem'),
-  cert: fs.readFileSync('./ssl/certificate.pem')
-};
- 
-const server    = http.createServer(app);
-const sslServer = https.createServer(sslOptions, app);
- 
-const redirectHttps = require('express-redirect-https');
-let redirectOptions = {
-  allowForwardForHeader: true,
-  httpsPort: 3043
-};
- 
-  
-// Or for a single
-app.get('/', redirectHttps(redirectOptions), function(request, response, next) {
-  response.send('https://portfolio.sarahbeecroft.me');
+var sslRedirect = require('heroku-ssl-redirect');
+var express = require('express');
+var app = express();
+
+// enable ssl redirect
+app.use(sslRedirect());
+
+app.get('/', function(req, res){
+  res.send('hello world');
 });
- 
- 
-server.listen(3000);
-sslServer.listen(3043);
+
+app.listen(3000);
