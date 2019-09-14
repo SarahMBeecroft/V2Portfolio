78da773516461c96a3cd78b47f6c9d468071cece
Sarah B <smblair87@gmail.com>
2019-09-13 18:53:09 -0700

    Updated styling

--- ssl.js
+++ ssl.js
@@ -1,6 +1,7 @@
 var sslRedirect = require('heroku-ssl-redirect');
 var express = require('express');
 var app = express();
+const port = 3000
 
 // enable ssl redirect
 app.use(sslRedirect());
@@ -9,4 +10,6 @@ app.get('/', function(req, res){
   res.send('hello world');
 });
 
-app.listen(3000);
+app.listen(port, () => console.log(`App listening on port ${port}!`))
+
+console.log('Listening on port ${port}!');
