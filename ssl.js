205759bad8b223625dbb41f5aabffdab495b6fd4
Sarah B <smblair87@gmail.com>
2019-09-14 19:38:55 -0700

    Trying simple-get npm package

--- ssl.js
+++ ssl.js
@@ -1,13 +1,7 @@
-var express = require('express');
-var http = require('http');
-var enforce = require('express-sslify');
+const get = require('simple-get')
  
-var app = express();
- 
-// Use enforce.HTTPS({ trustProtoHeader: true }) in case you are behind
-// a load balancer (e.g. Heroku). See further comments below
-app.use(enforce.HTTPS({ trustProtoHeader: true }),
- 
-http.createServer(app).listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
-});
+get('http://portfolio.sarahbeecroft.me', function (err, res) {
+  if (err) throw err
+  console.log(res.statusCode) // 200
+  res.pipe(process.stdout) // `res` is a stream
+})
